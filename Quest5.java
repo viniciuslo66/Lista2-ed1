f2e0d5d69fa1b20d3f7c6446e254d70e9da26d84
viniciuslo66 <viniciuslo66@gmail.com>
2022-06-13 07:47:39 -0300

    .

--- Quest5.java
+++ Quest5.java
@@ -1,24 +1,9 @@
-package lista2.quest5;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
-
-import lista2.quest3.linked.LinkedListSingly;
-
-//Lista de Exercícios da Primeira Unidade de Estrutura de Dados I
-
-//Aluno: Jacó Gonzaga Brasil Neto
-//Questão 5
-
-/*
-Desenvolva um programa que conte a frequência de cada palavra de um texto. Leia esse texto de um arquivo.
-Utilize uma lista simplesmente encadeada.
-Este programa deve permitir salvar o resultado em um arquivo binário e ler este arquivo binário.
-*/
 
 public class Quest5 {
 
@@ -39,7 +24,7 @@ public class Quest5 {
 			String[] textSplit = textInput.split(" "); // separa a frase em partes
 			String stringAux = ""; // string auxiliar
 			
-			LinkedListSingly<String> linkedListSingly = new LinkedListSingly<String>();
+			ListaDupla<String> linkedListSingly = new ListaDupla<String>();
 
 			for (int i = 0; i < textSplit.length; i++) {
 
@@ -60,7 +45,7 @@ public class Quest5 {
 							auxCont++;
 						}
 					}
-					linkedListSingly.addFirst(textSplit[i] + "," + auxCont + "\n"); // passa o resultado pra uma linked list
+					linkedListSingly.add(textSplit[i] + "," + auxCont + "\n"); // passa o resultado pra uma linked list
 					stringAux += textSplit[i] + " ";
 				}
 			}
@@ -91,7 +76,7 @@ public class Quest5 {
 		return sbResult.toString();
 	}
 
-	public static void writer(String path, LinkedListSingly<String> list) throws IOException 
+	public static void writer(String path, ListaDupla<String> list) throws IOException 
 	{
 		File file = new File(path);
     	file.delete();
